3e04355077790f0fa324221c3112303078de09d2
Kaisar Yerdenbekov <sysreqx@gmail.com>
2022-03-26 21:48:56 +0600

    initial commit

---
+++ src/main/java/kz/kaisar/springcrudapp02/Springcrudapp02Application.java
@@ -0,0 +1,13 @@
+package kz.kaisar.springcrudapp02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springcrudapp02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springcrudapp02Application.class, args);
+	}
+
+}
---
+++ src/test/java/kz/kaisar/springcrudapp02/Springcrudapp02ApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.kaisar.springcrudapp02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springcrudapp02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
